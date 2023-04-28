74a836edccf9d5ccdec0fbfd42a139d02b76db77
jisoo <doh_0112@naver.com>
2023-04-28 19:12:20 +0900

    working과 done 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Done from "./components/Done";
 import Header from "./components/Header";
+import Working from "./components/Working";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Working />
+      <Done />
     </div>
   );
 }
