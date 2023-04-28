142ad25fa5335498373e016dfb2c7fb4a8fac925
jisoo <doh_0112@naver.com>
2023-04-28 19:13:50 +0900

    dispatch로 addTodo 액션 크링에이터 사용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/modules/todoSlice";
+import { v4 as uuidv4 } from "uuid";
 
 const Header = () => {
   const [todo, setTodo] = useState({ title: "", content: "" });
+  const dispatch = useDispatch();
 
   // 입력
   const onChangeTodoHandler = (e) => {
     const { name, value } = e.target;
-    setTodo({ [name]: value });
+    setTodo({ ...todo, [name]: value });
   };
 
   // 추가
-  const onClickAddHandler = () => {};
+  const onClickAddHandler = () => {
+    const newTodo = {
+      title: todo.title,
+      content: todo.content,
+      id: uuidv4(),
+      isDone: false,
+    };
+    dispatch(addTodo(newTodo));
+  };
   return (
     <div>
-      Header
+      <h1>My TO DO LIST</h1>
       <input name="title" type="text" value={todo.title} onChange={onChangeTodoHandler} />
       <input name="content" type="text" value={todo.content} onChange={onChangeTodoHandler} />
-      {/* <button onClick={onClickAddHandler}>추가하기</button> */}
+      <button onClick={onClickAddHandler}>추가하기</button>
     </div>
   );
 };
